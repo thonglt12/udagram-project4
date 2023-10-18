fea30aad015c549d6f412ed68fd2c28d89284f1a
thonglt12 <thonglt1234@gmail.com>
2023-10-18 23:28:05 +0700

    add project

---
+++ udagram/udagram-api/src/config/config.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// ENV variables 
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
+
+export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+  session_token: process.env.SESSION_TOKEN,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
